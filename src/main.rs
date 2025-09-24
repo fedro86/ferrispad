92cfd7074f3364f5bea9634f77d57023c7e1f2ad
Federico Conticello <federico.conticello@hydroscan.be>
2025-09-24 11:21:40 +0200

    feat(open/save): use native open/save dialog

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,116 @@
 use fltk::{
-    app, dialog,
+    app,
+    dialog, // for alert_default
     enums::Color,
-    group::Flex, // Import the Flex layout widget
+    group::Flex,
     menu::MenuBar,
     prelude::*,
     text::{TextBuffer, TextEditor},
     window::Window,
 };
 use std::cell::RefCell;
-use std::rc::Rc;
 use std::fs;
+use std::rc::Rc;
+
+use fltk::dialog::{FileDialogType, NativeFileChooser};
+
+fn native_open_dialog(filter: &str) -> Option<String> {
+    let mut nfc = NativeFileChooser::new(FileDialogType::BrowseFile);
+    nfc.set_filter(filter);
+    nfc.show(); // returns (), blocks until close
+    let filename = nfc.filename();
+    let s = filename.to_string_lossy();
+    if s.is_empty() { None } else { Some(s.to_string()) }
+}
+
+fn native_save_dialog(filter: &str) -> Option<String> {
+    let mut nfc = NativeFileChooser::new(FileDialogType::BrowseSaveFile);
+    nfc.set_filter(filter);
+    nfc.show(); // returns (), blocks until close
+    let filename = nfc.filename();
+    let s = filename.to_string_lossy();
+    if s.is_empty() { None } else { Some(s.to_string()) }
+}
 
 fn main() {
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 640, 480, "RustPad");
+    let mut wind = Window::new(100, 100, 640, 480, "FerrisPad");
 
-    // Create a flexible column that will hold our widgets.
-    // This Flex container will fill the entire window.
     let mut flex = Flex::new(0, 0, 640, 480, None);
     flex.set_type(fltk::group::FlexType::Column);
 
-    // The MenuBar's size is now managed by Flex. We give it a fixed height.
     let mut menu = MenuBar::new(0, 0, 0, 30, "");
-    flex.fixed(&menu, 30); // Set fixed height for the menu bar
+    flex.fixed(&menu, 30);
 
     let text_buf = TextBuffer::default();
-    // The TextEditor's size is also managed by Flex. It will fill the remaining space.
     let mut text_editor = TextEditor::new(0, 0, 0, 0, "");
     text_editor.set_buffer(text_buf.clone());
 
-    flex.end(); // End of Flex group
-
-    // Tell the window that the 'flex' container is the resizable element.
+    flex.end();
     wind.resizable(&flex);
 
-    // --- State for line numbers ---
     let show_linenumbers = Rc::new(RefCell::new(false));
     text_editor.set_linenumber_bgcolor(Color::from_rgb(240, 240, 240));
     text_editor.set_linenumber_fgcolor(Color::from_rgb(100, 100, 100));
 
-    // --- File Menu ---
     let mut buf_new = text_buf.clone();
     let mut wind_new = wind.clone();
-    menu.add("File/New", fltk::enums::Shortcut::Ctrl | 'n', fltk::menu::MenuFlag::Normal, move |_| {
-        buf_new.set_text("");
-        wind_new.set_label("RustPad");
-    });
+    menu.add(
+        "File/New",
+        fltk::enums::Shortcut::Ctrl | 'n',
+        fltk::menu::MenuFlag::Normal,
+        move |_| {
+            buf_new.set_text("");
+            wind_new.set_label("FerrisPad");
+        },
+    );
 
+    // OPEN -> native dialog
     let mut buf_open = text_buf.clone();
     let mut wind_open = wind.clone();
-    menu.add("File/Open...", fltk::enums::Shortcut::Ctrl | 'o', fltk::menu::MenuFlag::Normal, move |_| {
-        if let Some(path) = dialog::file_chooser("Open File", "*.txt", ".", false) {
-            match fs::read_to_string(&path) {
-                Ok(content) => {
-                    buf_open.set_text(&content);
-                    wind_open.set_label(&format!("RustPad - {}", path));
-                },
-                Err(e) => dialog::alert_default(&format!("Error opening file: {}", e)),
+    menu.add(
+        "File/Open...",
+        fltk::enums::Shortcut::Ctrl | 'o',
+        fltk::menu::MenuFlag::Normal,
+        move |_| {
+            if let Some(path) = native_open_dialog("*.txt") {
+                match fs::read_to_string(&path) {
+                    Ok(content) => {
+                        buf_open.set_text(&content);
+                        wind_open.set_label(&format!("FerrisPad - {}", path));
+                    }
+                    Err(e) => dialog::alert_default(&format!("Error opening file: {}", e)),
+                }
             }
-        }
-    });
+        },
+    );
 
+    // SAVE AS -> native dialog
     let buf_save = text_buf.clone();
     let mut wind_save = wind.clone();
-    menu.add("File/Save As...", fltk::enums::Shortcut::Ctrl | 's', fltk::menu::MenuFlag::Normal, move |_| {
-        if let Some(path) = dialog::file_chooser("Save File As", "*.txt", ".", true) {
-            match fs::write(&path, buf_save.text()) {
-                Ok(_) => wind_save.set_label(&format!("RustPad - {}", path)),
-                Err(e) => dialog::alert_default(&format!("Error saving file: {}", e)),
+    menu.add(
+        "File/Save As...",
+        fltk::enums::Shortcut::Ctrl | 's',
+        fltk::menu::MenuFlag::Normal,
+        move |_| {
+            if let Some(path) = native_save_dialog("*.txt") {
+                match fs::write(&path, buf_save.text()) {
+                    Ok(_) => wind_save.set_label(&format!("FerrisPad - {}", path)),
+                    Err(e) => dialog::alert_default(&format!("Error saving file: {}", e)),
+                }
             }
-        }
-    });
+        },
+    );
 
-    menu.add("File/Quit", fltk::enums::Shortcut::Ctrl | 'q', fltk::menu::MenuFlag::Normal, move |_| {
-        app.quit();
-    });
+    menu.add(
+        "File/Quit",
+        fltk::enums::Shortcut::Ctrl | 'q',
+        fltk::menu::MenuFlag::Normal,
+        move |_| {
+            app.quit();
+        },
+    );
 
-    // --- View Menu ---
     let mut editor_clone = text_editor.clone();
     let linenumbers_state = show_linenumbers.clone();
     menu.add(
